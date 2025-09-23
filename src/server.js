dc26ef41b0312bc27e56eb3c456dcea85b446adc
Jean Lucas <j3anlucas010@gmail.com>
2025-09-23 10:28:20 -0300

    recuperando dados no body

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
 import http from 'node:http'
+import { buffer } from 'node:stream/consumers'
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
 
-    // Desestrutura as propriedades "method" (método HTTP) e "url" (rota requisitada) do objeto "req"
     const { method, url } = req
 
-    // Verifica se o método é GET e a rota é "/products"
     if (method === "GET" && url === '/products') {
-        // Se for, retorna uma resposta com a lista de produtos
         return res.end('Lista De Produtos!')
     }
-    // Caso contrário, verifica se o método é POST e a rota é "/products"
     else if (method === "POST" && url === '/products') {
-        // Se for, retorna uma resposta com status 201 (Criado) e confirma o cadastro do produto
-        return res.writeHead(201).end('Produto cadastrado !')
+
+        const buffers= []
+
+        for await(const chunk of req){
+            buffers.push(chunk)
+        }
+        console.log(Buffer.concat(buffers).toString())
+
     }
 
-    // Se nenhuma das condições acima for atendida, retorna status 404 (Não encontrado)
     return res.writeHead(404).end('Rota não encontrada!')
 })
 server.listen(3333)
